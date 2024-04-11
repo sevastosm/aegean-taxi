7611ac439f1822ca08bcbdf3e37577495053bd1d
Sevastos Matzouranis <sevastosmatzouranis@gmail.com>
2024-04-11 13:02:37 +0300

    Seo fixes

--- components/downloadAppButton.tsx
+++ components/downloadAppButton.tsx
@@ -68,7 +68,7 @@ export default function DownloadAppButton({
           <Image
             src={Apple}
             height={38}
-            alt="Download the Aegean Taxi App for Android"
+            alt="Download the Aegean Taxi App for iOS"
           />
         </a>
       );
@@ -97,7 +97,7 @@ export default function DownloadAppButton({
             <Grid item xs={3} md={2} sx={{ mt: 1 }}>
               <Image
                 src={at_logo_blue}
-                alt="Download the Aegean Taxi App for Android"
+                alt="Download the Aegean Taxi App for Android and iOS"
                 height={38}
               />
             </Grid>
@@ -125,72 +125,80 @@ export default function DownloadAppButton({
 
   return <ApplicationLink />;
 
-  return (
-    <>
-      <a href="https://aegean.page.link/navigate" target="_blank">
-        <Box
-          justifyContent="flex-start"
-          alignItems="flex-start"
-          display="flex"
-          sx={{
-            width: "100%",
-            height: "105px",
-            background: "#EBEBEB",
-            p: 2,
-            display: { xs: showXs, md: showMd },
-            mb: 2,
-            cursor: "pointer",
-          }}
-        >
-          <Grid container spacing={0} sx={{ background: "#fff", p: 1 }}>
-            <Grid item xs={3} md={2} sx={{ mt: 1 }}>
-              <Image src={at_logo_blue} alt="Aegean" height={38} />
-            </Grid>
-            <Grid item xs={7} md={8} sx={{ mt: 2, textAlign: "left" }}>
-              <Typography
-                component="span"
-                variant="body1"
-                gutterBottom
-                sx={{
-                  lineHeight: 1,
-                  mt: 0,
-                }}
-              >
-                Download the App
-              </Typography>
-            </Grid>
-            <Grid item xs={2} sx={{ mt: 1.5 }}>
-              <ArrowRightAltIcon fontSize="large" />
-            </Grid>
-          </Grid>
-        </Box>
-      </a>
+  // return (
+  //   <>
+  //     <a href="https://aegean.page.link/navigate" target="_blank">
+  //       <Box
+  //         justifyContent="flex-start"
+  //         alignItems="flex-start"
+  //         display="flex"
+  //         sx={{
+  //           width: "100%",
+  //           height: "105px",
+  //           background: "#EBEBEB",
+  //           p: 2,
+  //           display: { xs: showXs, md: showMd },
+  //           mb: 2,
+  //           cursor: "pointer",
+  //         }}
+  //       >
+  //         <Grid container spacing={0} sx={{ background: "#fff", p: 1 }}>
+  //           <Grid item xs={3} md={2} sx={{ mt: 1 }}>
+  //             <Image src={at_logo_blue} alt="Aegean" height={38} />
+  //           </Grid>
+  //           <Grid item xs={7} md={8} sx={{ mt: 2, textAlign: "left" }}>
+  //             <Typography
+  //               component="span"
+  //               variant="body1"
+  //               gutterBottom
+  //               sx={{
+  //                 lineHeight: 1,
+  //                 mt: 0,
+  //               }}
+  //             >
+  //               Download the App
+  //             </Typography>
+  //           </Grid>
+  //           <Grid item xs={2} sx={{ mt: 1.5 }}>
+  //             <ArrowRightAltIcon fontSize="large" />
+  //           </Grid>
+  //         </Grid>
+  //       </Box>
+  //     </a>
 
-      {/* App Stores */}
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "15px",
-          flexWrap: "wrap",
-        }}
-      >
-        {isAndroid() && (
-          <a
-            href="https://play.google.com/store/apps/details?id=aegean.taxi.ride.app.mykonos.santorini.athens.greece.passenger"
-            target="_blank"
-          >
-            <Image src={Google} height={38} alt="Google Play" />
-          </a>
-        )}
-        {isIOS() && (
-          <a href="https://apps.apple.com/app/id6447252101" target="_blank">
-            <Image src={Apple} height={38} alt="Apple Store" />
-          </a>
-        )}
-      </div>
-      {/* /. App Stores */}
-    </>
-  );
+  //     {/* App Stores */}
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //         gap: "15px",
+  //         flexWrap: "wrap",
+  //       }}
+  //     >
+  //       {isAndroid() && (
+  //         <a
+  //           href="https://play.google.com/store/apps/details?id=aegean.taxi.ride.app.mykonos.santorini.athens.greece.passenger"
+  //           target="_blank"
+  //         >
+  //           <Image
+  //             src={Google}
+  //             height={38}
+  //             alt="Download the Aegean Taxi App for Android"
+  //           />
+  //         </a>
+  //       )}
+  //       {isIOS() && (
+  //         <a href="https://apps.apple.com/app/id6447252101" target="_blank">
+  //           <Image
+  //             src={Apple}
+  //             height={38}
+  //             alt="Download the Aegean Taxi App for iOS"
+  //           />
+  //         </a>
+  //       )}
+  //     </div>
+  //     {/* /. App Stores */}
+  //   </>
+  // );
 }
