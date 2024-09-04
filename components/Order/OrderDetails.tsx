45989a3aab6ae326337e3ffa75bb2151222e80ed
Sevastos Matzouranis <sevastosmatzouranis@gmail.com>
2024-09-04 11:01:56 +0300

    Added new ride button

--- components/Order/OrderDetails.tsx
+++ components/Order/OrderDetails.tsx
@@ -1,5 +1,6 @@
 import { OrderData } from "@/types/bookingFlow";
 import moment from "moment";
+import Link from "next/link";
 import React from "react";
 import { json } from "stream/consumers";
 import CancelButton from "../CancelButton";
@@ -51,8 +52,8 @@ function OrderDetails({ screen, orderData, orderDetails, orderId }: Props) {
         <div>
           <div className="flex items-center justify-between mx-4 pt-8 pb-6 relative border-b-4 border-[#1218241a]">
             <div className="border-l-4 border-[#47C69C] min-h-[70px] ps-2 flex flex-col justify-between">
-              <p className="text-black text-[24px] font-bold">Pick up</p>
-              <p className="text-black font-normal text-[18px]">
+              <p className="text-black text-[16px] font-bold">Pick up</p>
+              <p className="text-black font-normal text-[15px]">
                 {orderData.waypoints[0].street}
               </p>
             </div>
@@ -60,8 +61,8 @@ function OrderDetails({ screen, orderData, orderDetails, orderId }: Props) {
               <img src={arrow} alt="Arrow" className="w-[100px] h-[15px]" />
             </div>
             <div className="border-l-4 border-black min-h-[70px] ps-2 flex flex-col justify-between">
-              <p className="text-black text-[24px] font-bold">Drop off</p>
-              <p className="text-black font-normal text-[18px]">
+              <p className="text-black text-[16px] font-bold">Drop off</p>
+              <p className="text-black font-normal text-[15px]">
                 {orderData.waypoints[1].street}
               </p>
             </div>
@@ -91,30 +92,38 @@ function OrderDetails({ screen, orderData, orderDetails, orderId }: Props) {
         </div>
       )}
       {screen === "reservation-received" && (
-        <div className="flex flex-col items-center justify-between flex-grow mx-4 my-4">
+        <div className="flex flex-col gap-2 items-center mx-4 my-4">
           <div className="flex items-center justify-center">
             <h1 className="font-bold text-xl text-center text-[#FF001F]">
               This is NOT a booking confirmation!
             </h1>
           </div>
           <div className="flex items-center justify-center">
-            <p className="font-semibold text-lg text-center">
+            <p className="font-semibold text-[17px] text-center">
               Once a driver accepts your ride, <br></br>this section will be
               updated <br></br>with driver details.
             </p>
           </div>
           <div className="flex items-center justify-center">
-            <p className="font-semibold text-xl text-center">
+            <p className="font-semibold text-[18px] text-center">
               Please check again using the link<br></br> provided on the SMS
             </p>
           </div>
         </div>
       )}
       {orderData.car && <DriverDetails details={orderDetails} />}
-      {screen !== "reservation-cancelled" && (
-        <div className="flex items-end justify-center my-4 px-2">
+      {screen !== "reservation-cancelled" ? (
+        <div className="flex items-end justify-center px-2">
           <CancelButton orderId={orderId} />
         </div>
+      ) : (
+        <Link href="/">
+          <div className="flex flex-col flex-grow items-center my-4 px-4">
+            <button className="bg-[#E8E8E8] rounded-lg py-3 font-bold text-lg text-blue-700 w-full mx-2">
+              New Ride
+            </button>
+          </div>
+        </Link>
       )}
     </div>
   );
