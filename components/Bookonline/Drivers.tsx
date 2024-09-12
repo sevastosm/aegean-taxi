db923a9a3c428383508308487c06adc7b47553fd
Seb <sevastosmatzouranis@gmail.com>
2024-09-12 16:19:47 +0300

    style fix

--- components/Bookonline/Drivers.tsx
+++ components/Bookonline/Drivers.tsx
@@ -14,21 +14,25 @@ export default function Drivers() {
   return (
     <div className="flex relative md:gap-2 flex-col md:flex-row min-h-[200px] max-w-[1200px] h-[calc(100vh-70px)] md:pt-4  mx-auto">
       <div className="absolute top-0 pt-3  left-0 z-10 w-full">
-        <ToolBar toggleDrawer={toggleDrawer} isMapOpen={open} />
+        <ToolBar
+          toggleDrawer={toggleDrawer}
+          isMapOpen={open}
+          colapsed={false}
+        />
       </div>
 
       <div
         className={classNames(
           "w-full md:h-[700px] md:ml-4",
           "relative",
-          !open ? "flex flex-grow h-[200px] min-h-[200px]" : "hidden md:block"
+          !open ? "flex h-[200px] min-h-[200px]" : "hidden md:block"
         )}
       >
-        <MapComponent />,
+        <MapComponent colapsed={false} />,
       </div>
       <div
         className={classNames(
-          "flex flex-col flex-grow gap-4 px-4 pb-4 min-w-auto md:min-w-[400px] md:h-[700px]",
+          "flex flex-col flex-grow gap-4 px-4 pb-4 min-w-auto md:min-w-[400px] md:h-[700px] justify-end md:justify-start md:flex-grow-0",
           open && "mt-12  md:mt-0 grow md:grow-0"
         )}
       >
--- components/Bookonline/ToolBar.tsx
+++ components/Bookonline/ToolBar.tsx
@@ -5,7 +5,11 @@ import NavBack from "./NavBack";
 
 type Props = {};
 
-const ToolBar = ({ toggleDrawer, isMapOpen }: any) => {
+const ToolBar = ({
+  toggleDrawer = () => false,
+  isMapOpen,
+  colapsed = true,
+}: any) => {
   const style = classNames(
     "pointer",
     "h-[50px] w-[50px]",
@@ -26,11 +30,13 @@ const ToolBar = ({ toggleDrawer, isMapOpen }: any) => {
   return (
     <div className="flex md:hidden justify-between align-baseline px-4">
       <NavBack handleClick={handleClick} />
-      <div onClick={click} className={style}>
-        <svg viewBox="0 0 24 24" aria-hidden="true">
-          <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6z"></path>
-        </svg>
-      </div>
+      {colapsed && (
+        <div onClick={click} className={style}>
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6z"></path>
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
